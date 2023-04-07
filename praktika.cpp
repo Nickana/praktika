c07f33725c44f63b1ee0b01c2c2f21343de50b4c
Aleksandr <marshsnader@yandex.ru>
2023-04-07 12:22:59 +0300

    Add project files.

---
+++ praktika.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+bool isPolindrom(std::string word)
+{
+	std::cout << "how fo you want to check for a polindrome, 1 - with other variable or 2 - with comprasion alghorithm: ";
+	std::string inverseWord;
+	int choose;
+	std::cin >> choose;
+	switch (choose) 
+	{
+	case 1:
+		for (int i = word.size() - 1; i >= 0; i--)
+		{
+			inverseWord += word[i];
+		}
+
+		std::cout << inverseWord;
+
+		if (inverseWord == word) {
+			return(true);
+		}
+		else
+		{
+			return(false);
+		}
+		break;
+	case 2:
+		for (int i = 0; i < (word.size() / 2) - 1; i++)
+		{
+			if (!(word[i] == word[(word.size() - 1) - i]))//блок будет выполнит только тогда когда найдется несовпадение, вернется false, а 
+			{											  // ! - инвертирует false в true что даст if выполнить блок кода. 
+				return(false);
+			}
+		}
+		return(true);
+	}
+}
+
+int main()
+{
+	std::string word;
+	std::cout << "type some word: ";
+	std::cin >> word;
+
+	std::cout << std::endl << isPolindrom(word);
+
+}
+
